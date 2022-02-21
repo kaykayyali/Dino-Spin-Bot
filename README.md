57549fa835cd5b98ce181a1684de35a1387b2da3
Kaysser Kayyali <kaykayyali@gmail.com>
2022-02-20 21:18:58 -0500

    Make the readme a little better, reminder to use dotenv locally.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Dino-Spin-Bot
 We play Ark.
 Sometimes we want to pick a random dino. I got bored, built this for fun. lets host it on heroku to waste some $$
+
+Using dotEnv for managing the token and config https://www.npmjs.com/package/dotenv
+Left an example
--- index.js
+++ index.js
@@ -17,7 +17,7 @@ client.once('ready', () => {
 });
 
 client.on('messageCreate', message => {
-  // Dependent on ROLLED the Wheel! being entered in mee6
+  // Dependent on Wants a new random dino being entered in mee6
 	if (message.content.includes("Wants a new random dino") || message.content.includes("TEST_SPIN_BOT")) {
     let outputString = spinner.random_dino();
     message.reply({ content: `${outputString}`, fetchReply: true });
